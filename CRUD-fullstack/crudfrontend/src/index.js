5bf740402dcebe5aa9c3e1de99a65b0b82318120
smeenguyen <nguyenvanquanghuy99@gmail.com>
2023-03-22 20:44:13 +0700

    crud fullstack

--- CRUD-fullstack/crudfrontend/src/index.js
+++ CRUD-fullstack/crudfrontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
